146f617fa30e4d139d323005b3393b3195879668
waywho <walzerfan@yahoo.com>
2018-02-02 18:13:35 +0000

    allow user to reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+  
+  private
   def authenticate_fellow!
 	unless current_lens_shifter.try(:admin?)
 		flash[:error] = "Sorry, this is a fellow only section"
