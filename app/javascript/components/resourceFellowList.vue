e9ac6a71166b724d10c2549ad5c7f90049b8f4c1
waywho <walzerfan@yahoo.com>
2018-03-30 23:45:01 +0100

    add unpublish date function

--- app/javascript/components/resourceFellowList.vue
+++ app/javascript/components/resourceFellowList.vue
@@ -22,6 +22,20 @@ export default {
             console.log(error)
             event.target.disabled = false
           })
+    },
+    unpublishNow: function (id, event) {
+      event.target.disabled = true
+      var meta = document.getElementsByTagName('meta')
+      var token = meta['csrf-token'].content
+      let now = new Date();
+      axios.patch(`/fellow/resource_items/${id}.json`, {'utf8': '✓', authenticity_token: token, resource_item: {published_at: ""}}).then(res => {
+            console.log(res)
+            this.$refs['resourceStatus' + res.data.id].innerHTML = "<button class='button table-button small' v-on:click='publishNow(<%= resource_item.id %>, $event)'>Publish</button>"
+            this.$refs['resourcePublishDate' + res.data.id].innerHTML = ""
+          }, error => {
+            console.log(error)
+            event.target.disabled = false
+          })
     }
   },
   created () {
