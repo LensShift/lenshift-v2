39a7460b8000fc911d3acb60dc72edd6185ba7fb
waywho <walzerfan@yahoo.com>
2018-04-01 11:37:08 +0100

    change stream layout

--- app/javascript/components/streamResource.vue
+++ app/javascript/components/streamResource.vue
@@ -1,29 +1,22 @@
 <template>
     <div class="lesson-container">
-        <div v-if="safeURL">
-        <div v-if="resource.resource_type === 'video' || resource.resource_type === 'podcast'" class="content-container" v-html="resource.video_embed"></div>
 
-        <div  v-else-if="resource.resource_type === 'graphic'" class="content-container"><img :src="resource.image.url"></div>
-
-        <div v-else-if="resource.no_iframe === true">
-          <span v-html="resource.analysis_content"></span>
-            <a :href="resource.source_url" alt="resource link" class="button" target="_blank">Click here to read more about it</a>
-        </div>
-
-        <div v-else>
-          <div class="iframe-head"><b>from</b> {{origin}}</div>
-          <iframe :srcdoc="resource.article_content" class="resource-iframe"></iframe>
-        </div>
-      </div>
-      <div v-else>
         <div class="card stream-resource-card">
-          <div class="card-divider">{{resource.title}}</div>
+          <div class="card-divider"><h6>From LensShift Library on:  {{resource.title}}</h6></div>
             <div class="card-section">
                 <span v-html="resource.analysis_content"></span>
-                <a :href="resource.source_url" alt="resource link" class="button" target="_blank">Click here to read more about it</a>
+ 
+                <div v-if="resource.resource_type === 'video' || resource.resource_type === 'podcast'" class="content-container" v-html="resource.video_embed"></div>
+
+                <div  v-else-if="resource.resource_type === 'graphic'" class="content-container"><img :src="resource.image.url"></div>
+
+                <div v-else>
+                  <br />
+                    <a :href="resource.source_url" alt="resource link" class="button" target="_blank">Click here to see the original content</a>
+                </div>
             </div>
         </div>
-      </div>      
+    
   </div>
 </template>
 
