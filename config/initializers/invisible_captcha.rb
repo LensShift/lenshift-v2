b103eaf50f23c41da4a14240ce5a149dfad04662
RMonteR <talktotheowl@inventati.org>
2019-08-08 09:50:24 +0200

    add invisible captcha to sessions

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,6 +1,6 @@
 InvisibleCaptcha.setup do |config|
   # config.honeypots           << ['more', 'fake', 'attribute', 'names']
-  config.visual_honeypots    = false
+  # config.visual_honeypots    = false
   # config.timestamp_threshold = 4
   # config.timestamp_enabled   = true
   # config.injectable_styles   = false
