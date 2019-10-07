cb3140ffd22715a0da444c5edc249cfa203cb1ac
RMonteR <talktotheowl@inventati.org>
2019-10-07 09:26:13 +0200

    Revert "override display:block rule for invisible captcha"
    This reverts commit 2228de122a9ab006437724cbab863f4bbfe0c9a5.

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -6,6 +6,6 @@ InvisibleCaptcha.setup do |config|
   # config.injectable_styles   = false
 
   # Leave these unset if you want to use I18n (see below)
-  config.sentence_for_humans = 'Please ignore this field'
+  config.sentence_for_humans     = 'Please ignore this field'
   # config.timestamp_error_message = 'Sorry, that was too quick! Please resubmit.'
 end
